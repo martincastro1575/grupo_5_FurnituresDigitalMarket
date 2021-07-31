dcfc297d94fdcd04830e842180b3adb924cab54b
Martin <martin.castro1575@gmail.com>
2021-07-30 21:52:17 -0300

    Mejoras formulario de registro y se crea ruta

--- routers/users.js
+++ routers/users.js
@@ -4,6 +4,7 @@ const routerUser = express.Router();
 const userController = require('../controllers/usersControllers');
 // const router = require('./products');
 
-routerUser.get('/login', userController.userLogin)
+routerUser.get('/login', userController.userLogin);
+routerUser.get('/registro', userController.usersAdd);
 
-module.exports = routerUser
+module.exports = routerUser;
