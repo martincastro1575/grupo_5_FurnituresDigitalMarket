e8e4eed94c53345679b6395dfa516a5e85b5408d
Martin <martin.castro1575@gmail.com>
2021-08-12 20:54:18 -0300

    Index mejorado, ruta index, controller index

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsPath = path.join(__dirname, '../data/products.json');
+let products = JSON.parse(fs.readFileSync(productsPath, 'utf-8'))
+
 const mainController = {
     'home': (req, res) => {
+        let producthotSales = products.filter(product =>
+                product.category == 'hotSales'
+            )
+        
+        let productsInSales = products.filter(product =>
+                product.category == 'inSale'
+            )
+        
+        let newProducts = products.find(product =>
+                product.category == 'newSales'
+            )
+
         return res.render('home', {
+            producthotSales: producthotSales,
+            productsInSales: productsInSales,
+            newProducts: newProducts,
             title: 'Home FDMk'
         });
     },
--- routers/products.js
+++ routers/products.js
@@ -2,11 +2,20 @@ const express = require('express');
 // const producController = require('../controllers/productsController');
 const router = express.Router();
 
+//declaramos el middleware a nivel de ruta
+const logMiddleWareDB = require('../middlewares/logDBMiddleware')
+
 const productController = require('../controllers/productsController');
 
 
 router.get('/editar', productController.productsEdit)
+
+
 router.get('/agregar', productController.producstAdd)
+
+//colocamos el middleware entre la ruta y el controller
+router.post('/agregar', logMiddleWareDB, productController.producstAdd)
+
 router.get('/listado', productController.productList)
 
 
