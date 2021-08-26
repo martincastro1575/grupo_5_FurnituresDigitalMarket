0b4b69adb731ffb55f61cdbaf1a4032e69d466ff
Martin <martin.castro1575@gmail.com>
2021-08-26 07:53:09 -0300

    Agregando persistencia de datos al formulario de registro

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -62,6 +62,7 @@ const userController = {
             
             res.render('users/register',{
                 errors: resultErros.mapped(),
+                oldData: req.body,
                 title: 'Registro de Usuario'
             })
         }
