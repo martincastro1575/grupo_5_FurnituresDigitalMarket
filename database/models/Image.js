a48510744328b4eb6b7ddc8b2494a4925be57a35
Martin <martin.castro1575@gmail.com>
2021-09-25 11:09:56 -0300

    Definiendo alias, cols y config del modelo Image

--- database/models/Image.js
+++ database/models/Image.js
@@ -2,12 +2,26 @@ module.exports=(sequelize, dataTypes)=>{
     let alias = 'Image';
 
     let cols = {
+        id:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
 
+        name:{
+            type: dataTypes.STRING(500),
+            allowNull: false,
+        },
+
+        id_product:{
+            type: dataTypes.INTEGER,
+        }
 
     }
 
     let config = {
-
+        tableName: 'image',
+        idProduct: 'id_product'
     }
 
 
