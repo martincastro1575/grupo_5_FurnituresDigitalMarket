0a3a762636e9bb547fd2f5b8787bef3a0c4a85ca
Martin <martin.castro1575@gmail.com>
2021-09-25 10:03:41 -0300

    Definiendo alias, cols y config del modelo product

--- database/models/Product.js
+++ database/models/Product.js
@@ -2,15 +2,74 @@ module.exports=(sequelize, dataTypes)=>{
     let alias = 'Product';
 
     let cols = {
+        id:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+
+        name:{
+            type: dataTypes.STRING(100),
+            allowNull: false,
+        },
+
+        description:{
+            type: dataTypes.TEXT,
+            allowNull: false,
+        },
+
+        price:{
+            type: dataTypes.DECIMAL(10,2),
+            allowNull: false,
+        },
+
+        discount:{
+            type: dataTypes.INTEGER,
+            defaultValue: true,
+        },
+
+        quantity:{
+            type: dataTypes.INTEGER,
+            defaultValue: true,
+            allowNull: false,
+        },
+
+        stock_min:{
+            type: dataTypes.INTEGER,
+            defaultValue: true,
+            allowNull: false,
+        },
+
+        stock_max:{
+            type: dataTypes.INTEGER,
+            defaultValue: true,
+            allowNull: false,
+        },
+
+        id_status:{
+            type: dataTypes.INTEGER,
+        },
+
+        id_category:{
+            type: dataTypes.INTEGER,
+        },
+
+        id_measure:{
+            type: dataTypes.INTEGER,
+        },
+
+        created_at:{
+            type:dataTypes.TIMESTAMP,
+        },
 
 
     }
 
     let config = {
-
+        timestamps: true,
+        createdAt: 'created_at',
+        tableName: 'products',
     }
-
-
 
     const Product = sequelize.define(alias, cols, config)
 
