6aeafd74c0ddf6395dc60bc1cd9137c05120a265
Martin <martin.castro1575@gmail.com>
2021-09-25 11:18:14 -0300

    Definiendo alias, cols y config del modelo action_type

--- database/models/ActionType.js
+++ database/models/ActionType.js
@@ -2,11 +2,31 @@ module.exports=(sequelize, dataTypes)=>{
     let alias = 'ActionType';
 
     let cols = {
+        id:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
 
+        name:{
+            type: dataTypes.STRING(50),
+            allowNull: false,
+        },
 
+        description:{
+            type: dataTypes.STRING(50),
+            allowNull: false,
+        },
+
+        created_at:{
+            type:dataTypes.TIMESTAMP,
+        },
     }
 
     let config = {
+        timestamps: true,
+        tableName: 'action_type',
+        createdAt: 'created_at',
 
     }
 
