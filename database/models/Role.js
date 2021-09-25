bac3fa361d1cbf86964a854be2698d5b28f2cd98
Martin <martin.castro1575@gmail.com>
2021-09-25 13:47:22 -0300

    Definiendo alias, cols y config del modelo Rol

--- database/models/Role.js
+++ database/models/Role.js
@@ -2,12 +2,20 @@ module.exports=(sequelize, dataTypes)=>{
     let alias = 'Role';
 
     let cols = {
-
+        id:{
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name:{
+            type: dataTypes.STRING(12),
+            allowNull: false,
+        },
 
     }
 
     let config = {
-
+        tableName: 'roles'
     }
 
 
