55847e3b9152a46f945c8c965ae27b114a569d32
Martin <martin.castro1575@gmail.com>
2021-10-02 12:32:21 -0300

    Para subir imagenes solo de productos

---
+++ middlewares/multerProductsMiddlewares.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const multer = require('multer');
+
+
+    const storage = multer.diskStorage({
+        destination: (req, file, cb)=>{
+            cb(null, path.join(__dirname, '../public/images/products'))
+        },
+        filename: (req, file, cb)=>{
+            const newFilename = 'prod-' + Date.now() + path.extname(file.originalname);
+            //console.log("Estoy en Multer " + newFilename);
+            cb(null, newFilename);
+        },
+    
+    })
+
+    //Ejecuto multer
+    const upload = multer({ storage: storage });
+    
+    module.exports = upload;
