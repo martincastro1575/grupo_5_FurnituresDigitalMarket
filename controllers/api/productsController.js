8a4a33a28062ac2140640414d90aad639ff7bb74
Martin <martin.castro1575@gmail.com>
2021-11-04 07:51:28 -0300

    Excluyendo campos y agregando propiedad detail

--- controllers/api/productsController.js
+++ controllers/api/productsController.js
@@ -6,18 +6,25 @@ const productsController = {
         const products = await db.Product.findAll({
             include: [{
                 model: db.ProductCategory,
-                as: 'categories'
-            }],
+                as: 'categories',
+                attributes:{
+                    exclude:['is_active','createdAt']
+                }
+            }]    
+            ,
             attributes: {
                 exclude:[
                     'high','width','length','price','discount','quantity','stock_min',
-                    'stock_max', 'idCategory','idStatus','created_at','id_category'
+                    'stock_max', 'idCategory','idStatus','created_at','id_category',
+                    
                 ]
             }
         })
+        
         products.forEach(product => {
-            console.log(product.dataValues.id)
+            product.dataValues.detail = `/images/products/${product.id}`
         });
+        
         return res.status(200).json({
             status: 200,
             total: products.length,
