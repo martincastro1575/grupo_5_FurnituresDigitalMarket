be9918e462980b5e96ee73e47691d7c46e8559af
Martin <martin.castro1575@gmail.com>
2021-11-08 06:11:44 -0300

    iniciando con paginacion de prodcutuctos

--- controllers/api/productsController.js
+++ controllers/api/productsController.js
@@ -6,7 +6,7 @@ const sequelize = db.sequelize;
 
 const productsController = {
     listado: async (req,res)=>{
-        const products = await db.Product.findAll({
+        const products = await db.Product.findAndCountAll({
             include: [{
                 model: db.ProductCategory,
                 as: 'categories',
@@ -21,9 +21,10 @@ const productsController = {
                     'high','width','length','price','discount','quantity','stock_min',
                     'stock_max', 'idCategory','idStatus','created_at','id_category',
                 ]},
-            
+            limit: 10,
+            offset:10,
         })
-
+        console.log(products)
         //Agrupando cantidad de productos por Categoria
         const prodBycats = await db.ProductCategory.findAll({
             attributes: ['description', [sequelize.fn('count',sequelize.col('products.id')),'Cantidad']],
@@ -33,7 +34,7 @@ const productsController = {
             }],
             group:['description']
         })
-        
+        //console.log(prodBycats)
         //Cantidad de prod por categoria
         let prodCategQty=[]
         prodBycats.forEach(prodCat => {
@@ -43,14 +44,18 @@ const productsController = {
             })
             
             });
-            // agregando detail
-        products.forEach(product => {
+        // agregando detail
+        products.rows.forEach(product => {
             product.dataValues.detail = `/images/products/${product.id}`
         });
         
+        //Total de Paginas
+        const pagesCount= Math.ceil(products.count / 10)
+
         return res.status(200).json({
             status: 200,
-            total: products.length,
+            productsByPage: products.rows.length,
+            pages: pagesCount,
             totalProdByCategory: prodCategQty,
             data: products,
         })
