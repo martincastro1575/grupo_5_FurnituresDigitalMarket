88b648bd47f685124867b8ae6f50aea4d0aa2c30
josibelhernandez <josiherz28@gmail.com>
2021-11-14 14:57:42 -0400

    User Details - api

--- controllers/api/usersApiController.js
+++ controllers/api/usersApiController.js
@@ -3,29 +3,55 @@ const Op = db.Sequelize.Op;
 
 module.exports = {
     list: (req, res) => {
-        db.User
-        .findAll(
-            {attributes: ['id', 'name', 'lastname', 'email']}
-        )
+        db.User.findAll({
+            attributes: ['id', 'name', 'lastname', 'email'],
+            raw: true
+        })
         .then(users =>{
-            return res.json({
+
+            users.forEach(user => {
+                user.userDetail = `http://localhost:3500/api/user/detail/${user.id}`
+            });
+
+                return res.json({
                 total: users.length,
                 data: users,
-                status: 200
-            })
+                status: 200,
+            }) 
         })
+
+  
        
     },
 
     searchById: (req, res) =>{
         db.User
         .findByPk(req.params.id)
-        .then(users =>{
+        .then(user =>{
+            
+            
+   
+            let userAvatar = 'http://localhost:3500/images/avatars/' + user.image      
+
             return res.json({
-                data: users,
-                status: 200
+                data: user,
+                status: 200,
+                userImg: userAvatar
             })
         })
     },
+
+    // API para ver el detalle de usuarios
+
+    detailUser: (req, res) => {
+        db.User.findByPk(req.params.id)
+        .then((users)=>{
+            res.json({
+                data: users,
+                status: 200
+            })
+            
+        })
+    }
 }
 
